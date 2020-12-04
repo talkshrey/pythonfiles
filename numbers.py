fb15eeede1fce8fdac8bb8695f5d24275c2f3a3d
talkshrey <74039736+talkshrey@users.noreply.github.com>
2020-12-04 14:20:20 +0530

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -1,5 +1,6 @@
 myDict = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
 myDict2 = {1: 'ten', 2: 'twenty', 3: 'thirty', 4: 'fourty', 5: 'fifty', 6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety'}
+myDict3 = {11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
 
 
 def individual_digits(num):
